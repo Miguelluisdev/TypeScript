031bdbf6ecea8b04253bf595e410234b20f5c28d
Miguel Luis <miguelluisataideferreira68@gmail.com>
2023-12-21 15:59:01 -0300

    a

--- evitando-tipagem.ts
+++ evitando-tipagem.ts
@@ -1,3 +1,15 @@
-function sendSpaceship(sapaceship: { pilot: string, copilot?: string})
+function sendSpaceship(sapaceship: { pilot: string, copilot?: string}){}
 
-// é possivel eviyar usando unknow ,never, any e etc.
+//z é possivel evitar usando unknow ,never, any e etc.
+
+// s primitivas: 
+// string
+// , 
+// number
+// , e
+// boolean
+// JavaScript tem três primitivas muito comumente usadas : string, number, e boolean. Cada um possui um tipo correspondente em TypeScript. Como seria de esperar, estes são os mesmos nomes que você veria se usasse o typeofoperador JavaScript em um valor desses tipos:
+
+// stringrepresenta valores de string como"Hello, world"
+// numberé para números como 42. JavaScript não possui um valor de tempo de execução especial para números inteiros, portanto não há equivalente para intou float- tudo é simplesmentenumber
+// booleané para os dois valores trueefalse
