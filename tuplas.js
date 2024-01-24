92b12804d38a9565010e1ecef8b819ec39aa14e9
Miguel Luis <miguelluisataideferreira68@gmail.com>
2024-01-23 23:37:07 -0300

    a

--- tuplas.js
+++
@@ -1,6 +0,0 @@
-var ResponseFalha = ["Não Encontrado", 404];
-var ResponseExitosa = ["{}", 200];
-if (ResponseExitosa[1] === 200) {
-    var InformacaoLocal = JSON.parse(ResponseExitosa[0]);
-    console.log(InformacaoLocal);
-}
